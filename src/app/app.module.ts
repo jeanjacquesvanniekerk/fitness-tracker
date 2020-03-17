ee505ebd31267d6220d2b4cb86307d2f2b545d84
Jean van Niekerk <jean.jacque@rocketmail.com>
2020-03-17 15:42:30 +0200

    Update Angular CLI, add sign up form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NewTrainingComponent } from "./training/new-training/new-training.compo
 import { PastTrainingComponent } from "./training/past-training/past-training.component";
 import { WelcomeComponent } from "./welcome/welcome.component";
 import { AppRoutingModule } from "./app-routing.module";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { AppRoutingModule } from "./app-routing.module";
     BrowserAnimationsModule,
     MaterialModule,
     AppRoutingModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
